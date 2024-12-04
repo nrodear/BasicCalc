be765efd2ecad4a1427d7b9cd7d27abe9837cbff
Nicolas Gerlach <nicolas.gerlach@gmx.de>
2024-12-04 12:50:37 +0100

    id: update

---
+++ standard/ButtonValues.cs
@@ -0,0 +1,52 @@
+﻿using CalcLib.Engine;
+using CalcLib.Types;
+
+namespace BasicCalc.Standard
+{
+    public class ButtonValues : IButtonValues
+    {
+        private readonly Item[,] _buttonItems = new Item[,] { };
+
+        public ButtonValues()
+        {
+            Item _nnnn = NoneItem.CreateItem();
+
+            Item Clear = Item.CreateItem("C", Methods.Clear());
+            Item Calc = Item.CreateItem("=", Methods.Result());
+
+            Item Add = Item.CreateItem("+", Methods.Add());
+            Item Minus = Item.CreateItem("-", Methods.Minus());
+
+
+            Item Num0 = Item.CreateItem("0", 0);
+            Item Num1 = Item.CreateItem("1", 1);
+            Item Num2 = Item.CreateItem("2", 2);
+            Item Num4 = Item.CreateItem("4", 4);
+            Item Num7 = Item.CreateItem("7", 7);
+
+
+            _buttonItems = new Item[,]
+            {
+                { _nnnn, Clear, _nnnn },
+                { Num4, Num7, Minus },
+                { Num1, Num2, Add },
+                { _nnnn, Num0,Calc },
+            };
+
+            GetLengthY = _buttonItems.GetUpperBound(0);
+            GetLengthX = _buttonItems.GetUpperBound(1);
+            GetX = GetLengthX + 1;
+        }
+
+        public int GetLengthY { get; }
+
+        public int GetLengthX { get; set; }
+
+        public int GetX { get; }
+
+        public Item GetItem(int y, int x)
+        {
+            return (Item)_buttonItems.GetValue(y, x);
+        }
+    }
+}
---
+++ standard/Methods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using CalcLib.Engine;
+using CalcLib.Types;
+
+namespace BasicCalc.Standard
+{
+    internal class Methods
+    {
+        internal static Method<float> Clear()
+        {
+            return SimpleBase;
+
+            [CalcType(CalcType.None)]
+            [LevelType(LevelType.None)]
+            [ArgsType(ArgsType.None)]
+            float SimpleBase(float[] f)
+            {
+                Values.Clear();
+                Elements.Clear();
+                return -1;
+            }
+        }
+
+        internal static Method<float> Result()
+        {
+            return SimpleBase;
+
+            [CalcType(CalcType.None)]
+            [LevelType(LevelType.None)]
+            [ArgsType(ArgsType.None)]
+            float SimpleBase(float[] f)
+            {
+                Values.Process();
+                Values.Clear();
+                return -1;
+            }
+        }
+
+        internal static Method<float> Add()
+        {
+            return SimpleBase;
+
+            [CalcType(CalcType.Func)]
+            [LevelType(LevelType.Two)]
+            [ArgsType(ArgsType.Two)]
+            float SimpleBase(float[] f)
+            {
+                ValidateArgs(f, ArgsType.Two);
+                return f[0] + f[1];
+            }
+        }
+
+        internal static Method<float> Minus()
+        {
+            return SimpleBase;
+
+            [CalcType(CalcType.Func)]
+            [LevelType(LevelType.Two)]
+            [ArgsType(ArgsType.Two)]
+            float SimpleBase(float[] f)
+            {
+                ValidateArgs(f, ArgsType.Two);
+                return f[0] - f[1];
+            }
+        }
+        
+        private static void ValidateArgs(float[] f, ArgsType  argType)
+        {
+            if (f != null && f.Length != (int)argType)
+            {
+                throw new ArgumentException("count args check failed");
+            }
+        }
+    }
+}
